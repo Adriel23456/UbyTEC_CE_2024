da01650e7de4f2c5d544b37da7016419d8b84ec1
Adriel23456 <adriel.chaves@hotmail.com>
2024-11-04 18:23:47 -0600

    Desarrollo de un detalle para los filtros completo

--- SQL_Server/obj/Debug/net8.0/SQL_Server.AssemblyInfo.cs
+++ SQL_Server/obj/Debug/net8.0/SQL_Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SQL_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32652c5fa204b625a231a60b7e19100b990469af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e73a899180472568f05ac2c2af87882d8d9b68e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SQL_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SQL_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
