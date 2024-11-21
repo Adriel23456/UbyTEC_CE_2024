9582fc33fed97bcc3394ce3d5c6e72892abb9295
Adriel23456 <adriel.chaves@hotmail.com>
2024-11-20 21:59:06 -0600

    Deploy importante a considerar

--- UbyTEC_Web/src/app/Components/BusinessManagerView/management-products/management-products.component.ts
+++ UbyTEC_Web/src/app/Components/BusinessManagerView/management-products/management-products.component.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import {ProductService,Product} from '../../../Services/Product/product.service';
-import { ComunicationService } from '../../../Services/Comunication/comunication.service';
-import { BusinessAssociateService } from '../../../Services/BusinessAssociate/business-associate.service';
+import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { map } from 'rxjs';
 
 @Component({
   selector: 'app-management-products',
