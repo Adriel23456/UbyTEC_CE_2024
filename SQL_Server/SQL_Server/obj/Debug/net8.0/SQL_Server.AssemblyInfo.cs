522684d5bd298c516f0ade97fe1dda7c11379948
Adriel23456 <adriel.chaves@hotmail.com>
2024-11-21 20:05:20 -0600

    Arreglo de un par de detalles

--- SQL_Server/SQL_Server/obj/Debug/net8.0/SQL_Server.AssemblyInfo.cs
+++ SQL_Server/SQL_Server/obj/Debug/net8.0/SQL_Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SQL_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f21bc0d485b451157405683682c18a40b2687fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d78eb8924055d2671c0964b16cda82e50f407197")]
 [assembly: System.Reflection.AssemblyProductAttribute("SQL_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SQL_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
